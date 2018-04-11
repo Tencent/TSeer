5162b58a8d32f0f86e1304b34a696a804f83f7d1
bertyoung <bertyoung666@gmail.com>
2018-04-11 10:02:19 +0800

    fix get local ip

--- build/utils/get_os_info.py
+++ build/utils/get_os_info.py
@@ -1,7 +1,21 @@
 # coding:utf8
 
 import socket
+import fcntl
+import struct
 
 
-def get_ip():
-    return socket.gethostbyname(socket.gethostname())
+def get_ip(ifname='eth0'):
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    ad = socket.inet_ntoa(fcntl.ioctl(
+        s.fileno(),
+        0x8915, # SIOCGIFADDR
+        struct.pack('256s', ifname[:15])
+        )[20:24])
+    s.close()
+    return ad
+
+   
+if __name__ == '__main__':
+    print get_ip()
+
