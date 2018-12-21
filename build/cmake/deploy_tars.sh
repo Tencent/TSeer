289c0a0bd249da685e559bf1c4f9363afff2266c
chhy2009 <chenhuiyong2009@gmail.com>
2018-12-21 01:46:03 -0800

    correct compile problem due to the directory structure adjustment of tars which is dependent on tseer

--- build/cmake/deploy_tars.sh
+++ build/cmake/deploy_tars.sh
@@ -6,7 +6,7 @@ if [ -n "Tars" -a -e "Tars" ];then
 	echo "[INFO] $TIMESTAMP we have already downloaded tars"
 else
     echo "[INFO] $TIMESTAMP downloading tars..."
-    git clone https://github.com/Tencent/Tars.git >/dev/null
+    git clone --recursive  https://github.com/Tencent/Tars.git >/dev/null
     if [ "$?" -ne "0" ]; then
         echo "[ERROR] $TIMESTAMP Download Tars failed."
         exit 2
@@ -17,9 +17,9 @@ fi
 # process Tars source file
 rm -f ./Tars/cpp/util/src/tc_gzip.cpp
 rm -f ./Tars/cpp/util/src/tc_mysql.cpp 
-sed -i '/framework/d' ./Tars/cpp/build/CMakeLists.txt 
-sed -i '/INSTALL/d' ./Tars/cpp/build/CMakeLists.txt 
-sed -i '/test/d' ./Tars/cpp/build/CMakeLists.txt 
+sed -i '/framework/d' ./Tars/cpp/CMakeLists.txt 
+sed -i '/INSTALL/d' ./Tars/cpp/CMakeLists.txt 
+sed -i '/test/d' ./Tars/cpp/CMakeLists.txt 
 
 # building
 mkdir -p ../thirdparty
@@ -27,7 +27,7 @@ INSTALLTARSPATH=$(pwd)/../thirdparty/tars/
 
 TIMESTAMP=`date "+%F %T"`
 echo "[INFO] $TIMESTAMP building tars into $INSTALLTARSPATH ..."
-cd ./Tars/cpp/build && cp CMakeLists.txt ../ && cmake .. -DCMAKE_INSTALL_PREFIX=$INSTALLTARSPATH && make && make install;
+cd ./Tars/cpp && cmake . -DCMAKE_INSTALL_PREFIX=$INSTALLTARSPATH && make && make install;
 if [ "$?" -ne "0" ]; then
 	echo "[ERROR] $TIMESTAMP Compile Tars failed."
 	exit 3
